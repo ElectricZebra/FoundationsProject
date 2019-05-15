e1a407b305f40d985b16d2a74538c2f11bce5fcd
Nicholas Regoli <nicholasregoli@gmail.com>
2019-05-14 19:19:14 -0700

    added css js html files

---
+++ foundationsProject.js
@@ -0,0 +1,26 @@
+console.log('js working')
+
+const data = {
+  customers: {
+    Dwight: {
+      Stapler: 0,
+      Candy: 0,
+      Coke: 0
+    },
+    Michael: {
+      Stapler: 0,
+      Candy: 0,
+      Coke: 0
+    },
+    Pam: {
+      Stapler: 0,
+      Candy: 0,
+      Coke: 0
+    }
+  },
+  prizes: {
+    Stapler: 1,
+    Candy: 3,
+    Coke: 5
+  }
+};
