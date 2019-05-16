c598cb1feff6c1e246407ea199b11dd7e7ab7a6f
Nicholas Regoli <nicholasregoli@gmail.com>
2019-05-16 15:20:12 -0700

    displaying customer prize count

--- foundationsProject.js
+++ foundationsProject.js
@@ -1,25 +1,25 @@
 const data = {
   customers: {
     Dwight: {
-      Stapler: 0,
-      Candy: 0,
-      Coke: 0
+      Stapler: 6,
+      Candy: 5,
+      Coke: 4
     },
     Michael: {
-      Stapler: 0,
-      Candy: 0,
-      Coke: 0
+      Stapler: 4,
+      Candy: 3,
+      Coke: 2
     },
     Pam: {
-      Stapler: 0,
-      Candy: 0,
-      Coke: 0
+      Stapler: 3,
+      Candy: 2,
+      Coke: 1
     }
   },
   prizes: {
     Stapler: 1,
-    Candy: 3,
-    Coke: 5
+    Candy: 5,
+    Coke: 7
   }
 };
 
@@ -27,6 +27,17 @@ const names = document.querySelector('#names');
 const prizes = document.querySelector('#prizes');
 const prize = data.prizes;
 const customers = data.customers;
+const arrCust = Object.keys(customers);
+const arrPrizes = Object.keys(prize);
+
+function custPrizeCount (name, prize) {
+  const countArr = [];
+  for (let key in data.customers[name]) {
+    countArr.push(data.customers[name][key]);
+  }
+  return countArr;
+}
+console.log(custPrizeCount("Michael"));
 
 const renderPrize = () => {
   return `
@@ -35,7 +46,7 @@ const renderPrize = () => {
     Object.keys(prize).map(key => `
       <li>
       ${ key} <div>
-      ${ data.prizes[key] }
+      ${ data.prizes[key]}
         </div>
       </li>
     `).join('')
@@ -46,24 +57,27 @@ const renderPrize = () => {
 prizes.innerHTML = renderPrize();
 
 const renderCustomer = () => {
-  const arrCust = Object.keys(customers);
   return `
   <ul>
     ${
-    arrCust.map(key => `
-        <li>${ key}</li>
-        <div id='customerPrizes'>
-        ${ Object.keys(prize).map(key => `
-        <li>
-        ${ key }
-        </li>
-        `).join('') }
-        </div>
-          `).join('')
+      arrCust.map(key => `
+        <li>${ key }</li>
+          <div id='customerPrizes'>
+            ${ Object.keys(prize).map(key2 => `
+              <li>${ key2 }</li>
+              <div id='prizeCount'>
+                ${ customers[key][key2] }
+              </div>
+            `).join('')}
+          </div>
+      `).join('')
     }
-
   </ul>
 `
 };
 
 names.innerHTML = renderCustomer();
+
+                // ${ arrCust.map( key3 =>
+                //   `${ custPrizeCount(key) }`
+                // )}
